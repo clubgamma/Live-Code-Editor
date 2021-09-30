9cfc495747eb1af2ab5f6dc335e2374932864d49
Ankit1598 <ankitchaudhari1598@gmail.com>
2021-10-01 02:04:06 +0530

    :building_construction: added live compile feature with ui effects

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 function compile() {
- 
+	var html = document.getElementById("html");
+	var css = document.getElementById("css");
+	var js = document.getElementById("js");
+	var code = document.getElementById("output").contentWindow.document;
+	document.body.onkeyup = function () {
+		code.open();
+		code.writeln(
+			html.value +
+				"<style>" +
+				css.value +
+				"</style>" +
+				"<script>" +
+				js.value +
+				"</script>"
+		);
+		code.close();
+	};
 }
 
 compile();
+
+document.querySelectorAll(".control").forEach((control) =>
+	control.addEventListener("click", (e) => {
+		e.target.parentElement.parentElement.classList.toggle("collapse");
+		e.target.classList.toggle("close");
+	})
+);
