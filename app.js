341934721c47b6e0a1a75ec11ff732c005d77cf1
Harshit Kumar <kumarharshit850@gmail.com>
2021-10-04 00:15:40 +0530

    UI Improved

--- app.js
+++ app.js
@@ -1,82 +1,108 @@
-const codeInputs = {
-  html: document.querySelector('#html'),
-  css: document.querySelector('#css'),
-  js: document.querySelector('#js')
-};
-
-const code = document.getElementById("output").contentWindow.document;
-
+var html = document.getElementById('html');
+var css = document.getElementById('css');
+var js = document.getElementById('js');
+var code = document.getElementById('output').contentWindow.document;
 function compile() {
-  const PREFIX = "livecode-";
-  const data = ["html", "css", "js"].map((key) => {
+  const PREFIX = 'livecode-';
+  const data = ['html', 'css', 'js'].map((key) => {
     const prefixedKey = PREFIX + key;
     const jsonValue = localStorage.getItem(prefixedKey);
+    console.log(jsonValue);
     if (jsonValue != null) return JSON.parse(jsonValue);
   });
-  codeInputs.html.value = data[0] || "";
-  codeInputs.css.value = data[1] || "";
-  codeInputs.js.value = data[2] || "";
-
+  setInitial(data);
   document.body.onkeyup = function () {
-    writeInIframe();
-
-    localStorage.setItem("livecode-html", JSON.stringify(codeInputs.html.value));
-    localStorage.setItem("livecode-css", JSON.stringify(codeInputs.css.value));
-    localStorage.setItem("livecode-js", JSON.stringify(codeInputs.js.value));
+    localStorage.setItem('livecode-html', JSON.stringify(html.value));
+    localStorage.setItem('livecode-css', JSON.stringify(css.value));
+    localStorage.setItem('livecode-js', JSON.stringify(js.value));
+    code.open();
+    code.writeln(
+      html.value +
+        '<style>' +
+        css.value +
+        '</style>' +
+        '<script>' +
+        js.value +
+        '</script>'
+    );
+    code.close();
   };
-  writeInIframe();
-};
-    
-function writeInIframe() {
+}
+
+function setInitial(data) {
+  let htmlContent = data[0] || '<h1>Welcome to the Live Code Editor!</h1>';
+  let cssContent =
+    data[1] ||
+    `body {
+    background-color: #222;
+    }
+    h1 {
+      color: #fff;
+      text-align: center;
+      margin-top: 10%;
+    }`;
+  let jsContent = data[2] || '';
+  css.value = cssContent;
+  console.log(cssContent);
+  js.value = jsContent;
+  html.value = htmlContent;
   code.open();
   code.writeln(
-    codeInputs.html.value +
-      "<style>" +
-      codeInputs.css.value +
-      "</style>" +
-      "<script>" +
-      codeInputs.js.value  +
-      "</script>"
-      );
+    htmlContent +
+      '<style>' +
+      cssContent +
+      '</style>' +
+      '<script>' +
+      jsContent +
+      '</script>'
+  );
   code.close();
-};
+}
 
-function displayMsgCopied(e) {
-  const temp = e.target.innerHTML;
-  e.target.innerText = 'Copied!';
-  setTimeout( () => {
-    e.target.innerHTML = temp
-  }, 800)
-};
+compile();
 
-function copyCode(code) {
-  code.select();
-  document.execCommand("copy");
-};
-  
-document.querySelectorAll(".control").forEach((control) =>
-  control.addEventListener("click", (e) => {
-    e.target.parentElement.parentElement.classList.toggle("collapse");
-    e.target.classList.toggle("close");
-    e.target.parentElement.querySelector("h2").classList.toggle("hidden");
+document.querySelectorAll('.control').forEach((control) =>
+  control.addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.classList.toggle('collapse');
+    e.target.classList.add('close');
+    e.target.parentElement.querySelector('h2').classList.toggle('hidden');
   })
 );
 
-document.querySelectorAll(".clear").forEach((clear) =>
-  clear.addEventListener("click", (e) => {
+document.querySelectorAll('.clear').forEach((clear) =>
+  clear.addEventListener('click', (e) => {
     const ele = e.target.classList[1];
-    document.querySelector(`#${ele}`).value = "";
-    localStorage.setItem(`livecode-${ele}`, JSON.stringify(""));
-
+    document.querySelector(`#${ele}`).value = '';
+    localStorage.setItem(`livecode-${ele}`, JSON.stringify(''));
     compile();
   })
 );
 
-for (let input of Object.keys(codeInputs)) {
-  document.querySelector(`.copy-${input}`).addEventListener('click', (e) => {
-    displayMsgCopied(e);
-    copyCode(codeInputs[input]);
-  })
-}
+document.querySelectorAll('.copy-btn').forEach((copy) => {
+  copy.addEventListener('click', (e) => {
+    const temp = e.target.innerHTML;
+    e.target.innerText = 'Copied!';
+    setTimeout(function () {
+      e.target.innerHTML = temp;
+    }, 800);
+  });
+});
 
-compile()
+document.querySelector('.copy-html').addEventListener('click', (e) => {
+  const code = document.querySelector('#html');
+  copyCode(code);
+});
+
+document.querySelector('.copy-css').addEventListener('click', (e) => {
+  const code = document.querySelector('#css');
+  copyCode(code);
+});
+document.querySelector('.copy-js').addEventListener('click', (e) => {
+  const code = document.querySelector('#js');
+  copyCode(code);
+});
+
+function copyCode(code) {
+  code.select();
+  document.execCommand('copy');
+}
